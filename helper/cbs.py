82f71170681b59e6e4d8b3cbf21866961b114c39
dzaiats <denys.zaiats@gmail.com>
2016-08-11 09:35:11 +0200

    [master] -added common methods

--- helper/cbs.py
+++ helper/cbs.py
@@ -1,11 +1,18 @@
 import os
+import random
 import subprocess
 import threading
+from time import sleep
+from testlio.base import TestlioAutomationTest
 
 local = threading.local()
 
 
-class CommonHelper:
+class CommonHelper(TestlioAutomationTest):
+
+    phone = False
+    tablet = False
+
     def init(self, driver):
         # stores the webdriver as a local thread attribute to avoid having to pass the webdriver everywhere as parameter
         local.driver = driver
@@ -93,14 +100,135 @@ class CommonHelper:
             count += 1
         local.driver.implicitly_wait(30)
 
-    def click_by_location(self, elem):
+    def open_drawer(self):
+        self.click(name='Open navigation drawer', screenshot=True)
+
+    def close_drawer(self):
+        self.driver.back()
+
+    def navigate_up(self):
+        self.click(name='Navigate up', screenshot=True)
+
+    def goto_sign_in(self):
+        self.open_drawer()
+
+        # on some screens (live tv), the text 'Sign In' appears twice, so be sure we get the right one...
+        drawer = self.driver.find_element_by_id('com.cbs.app:id/userInfoHolder')
+        sign_in = drawer.find_element_by_name('Sign In')
+        self.click(element=sign_in, data='Click on menu item Sign In', screenshot=True)
+        self._hide_keyboard()
+
+    def goto_sign_up(self):
+        self.click(name='Sign Up')
+        self._hide_keyboard()
+
+    def goto_home(self):
+        self.open_drawer()
+        self.go_to('Home')
+
+    def goto_shows(self):
+        self.open_drawer()
+        self.go_to('Shows')
+
+    def goto_live_tv(self):
+        self.open_drawer()
+        self.click(name="Live TV", screenshot=True)
+        if self.testdroid_device == "LGE Nexus 5 6.0":
+            try:
+                self.click_until_element_is_visible("Open navigation drawer", "Allow")
+            except:
+                pass
+        self.driver.implicitly_wait(120)
+
+    def goto_schedule(self):
+        self.open_drawer()
+        self.go_to('Schedule')
+
+    def goto_settings(self):
+        self.open_drawer()
+        self.go_to('Settings')
+
+    def goto_show(self, show_name):
+        self.click(id='com.cbs.app:id/action_search')
+        sleep(1)
+
+        e = self._find_element(id='com.cbs.app:id/search_src_text')
+        self.send_keys(show_name, e)
+        sleep(5)
+        try:
+            self.click(id='com.cbs.app:id/showImage', screenshot=True)
+            self.event.screenshot(self.screenshot())
+        except:
+            self.driver.tap([(220, 450)])
+            pass
+        sleep(5)
+        self._hide_keyboard()
+
+    def go_to(self, menu):
+        drawer = self._find_element(id='com.cbs.app:id/navigation_drawer')
+        self.click(element=drawer.find_element_by_name(menu), data='Click on menu item %s' % menu, screenshot=True)
+
+    def click_by_location(self, elem, **kwargs):
         """
         sometimes elem.click() fails for whatever reason.  get x,y coords and click by that
         """
         loc = elem.location
         size = elem.size
-        x = loc['x'] + size['width'] / 2
-        y = loc['y'] + size['height'] / 2
+        if self.tablet:
+            if kwargs['side'] == 'middle':
+                x = loc['x'] + size['width'] / 2
+                y = loc['y'] + size['height'] / 2
+
+            elif kwargs['side'] == 'left':
+                x = loc['x'] + size['width'] / 4
+                y = loc['y'] + size['height'] / 2
+
+            elif kwargs['side'] == 'right':
+                x = loc['x'] + size['width'] - 10
+                y = loc['y'] + 10
+
+        elif self.phone:
+            if kwargs['side'] == 'middle':
+                x = loc['x'] + size['width'] / 2
+                y = loc['y'] + size['height'] / 2
+
+            elif kwargs['side'] == 'left':
+                x = loc['x'] + size['width'] / 4
+                y = loc['y'] + size['height'] / 2
+
+            elif kwargs['side'] == 'right':
+                x = loc['x'] + size['width'] - size['width'] / 4
+                y = loc['y'] + size['height'] / 2
 
         # an array of tuples
         local.driver.tap([(x, y)])
+
+    def generate_random_string(self, length=8):
+        return str(''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for _ in range(length)))
+
+    def _hide_keyboard(self):
+        for x in range(0, 3):
+            try:
+                local.driver.hide_keyboard()
+                sleep(1)
+            except:
+                pass
+        sleep(2)
+
+    def _short_swipe_up(self, duration=1000):
+        size = local.driver.get_window_size()
+        x = size['width'] / 2
+        start_y = size['height'] / 2
+        end_y = size['height'] - 50
+
+        local.driver.swipe(x, start_y, x, end_y, duration)
+        sleep(1)
+
+    def _short_swipe_down(self, duration=4000):
+        size = local.driver.get_window_size()
+        x = size['width'] / 2
+        start_y = size['height'] / 2
+        end_y = 50
+
+        local.driver.swipe(x, start_y, x, end_y, duration)
+        sleep(1)
