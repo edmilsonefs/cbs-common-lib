1465aa96d139b56ea500c57d88a4d7ac8a6bc1a1
dzaiats <denys.zaiats@gmail.com>
2016-08-11 10:00:23 +0200

    [master] -added common methods

--- helper/cbs.py
+++ helper/cbs.py
@@ -5,17 +5,18 @@ import threading
 from time import sleep
 from testlio.base import TestlioAutomationTest
 
-local = threading.local()
-
+driver = None
 
 class CommonHelper(TestlioAutomationTest):
 
     phone = False
     tablet = False
 
-    def init(self, driver):
+    def __init__(self, d):
+        self.driver = d
+
+    def init(self):
         # stores the webdriver as a local thread attribute to avoid having to pass the webdriver everywhere as parameter
-        local.driver = driver
         self._identify_device()
 
     def get_hosting_platform(self):
@@ -73,32 +74,32 @@ class CommonHelper(TestlioAutomationTest):
         return local.is_amazon and local.is_tablet
 
     def click_until_element_is_visible(self, element_to_be_visible, element_to_click, click_function):
-        local.driver.implicitly_wait(20)
+        self.driver.implicitly_wait(20)
 
         element = None
         count = 0
         while element is None and count < 30:
             try:
-                element = local.driver.find_element_by_name(element_to_be_visible)
+                element = self.driver.find_element_by_name(element_to_be_visible)
             except:
                 click_function(name=element_to_click)
                 count += 1
 
-        local.driver.implicitly_wait(30)
+        self.driver.implicitly_wait(30)
 
     def go_to_menu_page_and_select_option(self, menu_option):
         # This is to avoid navigation drawer not being clicked properly
         count = 0
-        local.driver.implicitly_wait(10)
+        self.driver.implicitly_wait(10)
         while count < 30:
             try:
-                local.driver.find_element_by_name('Open navigation drawer').click()
-                local.driver.find_element_by_name(menu_option).click()
+                self.driver.find_element_by_name('Open navigation drawer').click()
+                self.driver.find_element_by_name(menu_option).click()
                 break
             except:
                 pass
             count += 1
-        local.driver.implicitly_wait(30)
+        self.driver.implicitly_wait(30)
 
     def open_drawer(self):
         self.click(name='Open navigation drawer', screenshot=True)
@@ -201,7 +202,7 @@ class CommonHelper(TestlioAutomationTest):
                 y = loc['y'] + size['height'] / 2
 
         # an array of tuples
-        local.driver.tap([(x, y)])
+        self.driver.tap([(x, y)])
 
     def generate_random_string(self, length=8):
         return str(''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for _ in range(length)))
@@ -209,26 +210,26 @@ class CommonHelper(TestlioAutomationTest):
     def _hide_keyboard(self):
         for x in range(0, 3):
             try:
-                local.driver.hide_keyboard()
+                self.driver.hide_keyboard()
                 sleep(1)
             except:
                 pass
         sleep(2)
 
     def _short_swipe_up(self, duration=1000):
-        size = local.driver.get_window_size()
+        size = self.driver.get_window_size()
         x = size['width'] / 2
         start_y = size['height'] / 2
         end_y = size['height'] - 50
 
-        local.driver.swipe(x, start_y, x, end_y, duration)
+        self.driver.swipe(x, start_y, x, end_y, duration)
         sleep(1)
 
     def _short_swipe_down(self, duration=4000):
-        size = local.driver.get_window_size()
+        size = self.driver.get_window_size()
         x = size['width'] / 2
         start_y = size['height'] / 2
         end_y = 50
 
-        local.driver.swipe(x, start_y, x, end_y, duration)
+        self.driver.swipe(x, start_y, x, end_y, duration)
         sleep(1)
