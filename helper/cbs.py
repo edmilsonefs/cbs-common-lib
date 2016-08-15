584fdc8140d4b9cf2d640ae6b28421ec8884c540
denys <denys.zaiats@gmail.com>
2016-08-15 13:01:41 +0200

    [main] - added fix for clicking on the first AA video on home page

--- helper/cbs.py
+++ helper/cbs.py
@@ -249,6 +249,7 @@ class CommonHelper(TestlioAutomationTest):
             prime_location = self._find_element(name='Primetime Episodes').location
             if prime_location['y'] > size['height'] / 2:
                 self._short_swipe_down(duration=5000)
+                sleep(5)
         list_episodes = self.driver.find_elements_by_xpath("//android.widget.LinearLayout[./android.widget.TextView[@text='Primetime Episodes']]//*[@resource-id='com.cbs.app:id/allAccessFlag']")
         count = 0
         while count < len(list_episodes):
