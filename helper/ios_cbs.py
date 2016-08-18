06eb3b22571cb9111d3aacea11ed6175bdf70bbd
dzaiats <denys.zaiats@gmail.com>
2016-08-18 12:05:27 +0200

    [main] - string the extending of reg-email-ios for using of common lib

--- helper/ios_cbs.py
+++ helper/ios_cbs.py
@@ -4,12 +4,14 @@ import subprocess
 from time import sleep
 
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 
 from testlio.base import TestlioAutomationTest
 
 class CommonIOSHelper(TestlioAutomationTest):
     phone = False
     tablet = False
+    needToAccept = True
     testdroid_device = os.getenv('TESTDROID_DEVICE')
     default_implicit_wait = 120
 
@@ -27,6 +29,7 @@ class CommonIOSHelper(TestlioAutomationTest):
     def teardown_method(self, method):
         # subprocess.call("adb shell am start -n io.appium.settings/.Settings -e wifi on", shell=True)
         super(CommonIOSHelper, self).teardown_method(method)
+        sleep(180)
 
     def find_by_uiautomation(self, value, hide_keyboard=False):
         return self.driver.find_element(By.IOS_UIAUTOMATION, value)
@@ -83,9 +86,9 @@ class CommonIOSHelper(TestlioAutomationTest):
 
     def go_to_show(self, show_name):
         self.go_to_shows()
-        self.click(id="Search")
-        self.send_text_native(show_name)
-        self.driver.tap([(50, 150)])
+        # self.click(id="Search")
+        # self.send_text_native(show_name)
+        self.driver.tap([(80, 170)])
 
     def _accept_alert(self, count):
         for x in range(0, count):
@@ -96,3 +99,56 @@ class CommonIOSHelper(TestlioAutomationTest):
                 break
             except:
                 pass
+
+    def _short_swipe_up(self, duration=1000, side='middle'):
+        size = self.driver.get_window_size()
+        if side == 'middle':
+            x = size['width'] / 2
+        elif side == 'left':
+            x = 50
+        elif side == 'right':
+            x = size['width'] - 50
+        start_y = size['height'] / 2
+        end_y = size['height'] - 50
+
+        self.driver.swipe(x, start_y, x, end_y, duration)
+        sleep(1)
+
+    def _short_swipe_down(self, duration=4000, side='middle'):
+        size = self.driver.get_window_size()
+        if side == 'middle':
+            x = size['width'] / 2
+        elif side == 'left':
+            x = 50
+        elif side == 'right':
+            x = size['width'] - 50
+        start_y = size['height'] / 2
+        end_y = 50
+
+        self.driver.swipe(x, start_y, x, end_y, duration)
+        sleep(1)
+
+    def click_on_first_aa_video(self):
+        # count = 1
+        # if self.phone:
+        #     max_count = 2
+        # else:
+        #     max_count = 3
+        # while count <= max_count:
+        self.click(
+            xpath="//UIATableCell[contains(@name,'Primetime')]//UIACollectionView[1]//UIACollectionCell[1]")
+        if self.needToAccept:
+            self._accept_alert(1)
+            self.needToAccept = False
+
+            # try:
+            #     self.event.screenshot(self.screenshot())
+            #     self.driver.find_element_by_id('TRY 1 WEEK FREE*')
+            # except:
+            #     self.event.screenshot(self.screenshot())
+            #     try:
+            #         self.driver.find_element_by_id('Cancel').click()
+            #     except:
+            #         self.driver.tap([(100, 100)])
+            #         self.driver.find_element_by_id('Done').click()
+            #         count += 1
