729609143df5af2ea2f29d456cc1fc98147819b5
Denys Zaiats <denys.zaiats@dreamit.de>
2017-01-04 10:59:46 +0100

    [master] - merged Android JW to common script

--- helper/ios_cbs.py
+++ helper/ios_cbs.py
@@ -205,7 +205,7 @@ class CommonIOSHelper(TestlioAutomationTest):
     def goto_live_tv(self):
         self.open_drawer()
         self.click(id='Live TV')
-        self._accept_alert(1)
+        self._accept_alert(2)
 
     def goto_schedule(self):
         self.open_drawer()
