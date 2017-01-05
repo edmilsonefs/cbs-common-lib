7e904f5ced4e111571c97f6299db14fcae74c284
Denys Zaiats <denys.zaiats@dreamit.de>
2017-01-05 15:07:06 +0100

    [reg-page-obj] - added upsell page

--- helper/android/upsell_page.py
+++ helper/android/upsell_page.py
@@ -7,11 +7,35 @@ class UpsellPage(BasePage):
     def __init__(self, driver, event):
         super(UpsellPage, self).__init__(driver, event)
 
-    def email(self, timeout=10):
-        return self.get_element(timeout=timeout, id=self.com_cbs_app + ':id/edtEmail')
-
     def already_subscriber_sign_in(self, timeout=10):
-        return self.get_element(timeout=timeout, id=self.com_cbs_app + ':id/edtEmail')
+        return self.get_element(timeout=timeout, name='Already a subscriber? Sign In')
+
+    def validate_page(self, user_type):
+        self.verify_exists(id=self.com_cbs_app + ':id/allAccessLogo', screenshot=True)
+        if user_type in [self.anonymous, self.registered]:
+            self.verify_exists(
+                xpath="//android.widget.TextView[contains(@text,'LIMITED') and contains(@text,'COMMERCIALS')]")
+            self.verify_exists(xpath="//*[contains(@text,'TRY 1 ') and contains(@text,' FREE') "
+                                     "and (contains(@text,'MONTH') or contains(@text,'WEEK'))]")
+            self.verify_exists(xpath="//*[contains(@text,'COMMERCIAL FREE')]")
+            self.verify_exists(name='GET STARTED')
+            if user_type == self.registered:
+                self.verify_not_exists(name='SELECT', timeout=10)
+        elif user_type in [self.subscriber, self.trial]:
+            self.verify_exists(xpath="//*[contains(@text,'COMMERCIAL FREE')]")
+            self.verify_exists(xpath="//*[contains(@text,'UPGRADE')]")
+        elif user_type == self.cf_subscriber:
+            self.verify_exists(xpath="//*[contains(@text,'COMMERCIAL FREE')]")
+        else:
+            if user_type == self.ex_subscriber:
+                self.verify_exists(
+                    xpath="//android.widget.TextView[contains(@text,'LIMITED') and contains(@text,'COMMERCIALS')]")
+                self.verify_exists(xpath="//*[contains(@text,'COMMERCIAL FREE')]")
+                self.verify_exists(xpath="//*[contains(@text,'Only $ 5.99/month')]")
+                self.verify_exists(name='SELECT')
+                self.verify_not_exists(xpath="//*[contains(@text,'TRY 1 ') and contains(@text,' FREE') "
+                                             "and (contains(@text,'MONTH') or contains(@text,'WEEK'))]", timeout=10)
+                self.verify_not_exists(name='GET STARTED', timeout=10)
 
     def select_sign_in_from_text_link(self):
         self.event._log_info(self.event._event_data('Select Sign In'))
