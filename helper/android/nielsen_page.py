104394cd0315481660236b6652355c3b021a5b6b
luisatheodoro <theodoro.luisa@gmail.com>
2017-01-16 09:08:11 +0000

    nielsen

--- helper/android/nielsen_page.py
+++ helper/android/nielsen_page.py
@@ -1,4 +1,5 @@
 from helper.android.base_page import BasePage
+from time import sleep
 
 
 class NielsenPage(BasePage):
@@ -16,6 +17,15 @@ class NielsenPage(BasePage):
 
     def btn_ok(self, timeout=10):
         return self.get_element(timeout=timeout, name='OK')
+
+    def goto_nielsen_opt_out(self):
+        self.goto_nielsen_info_page()
+
+        self.click(element=self.btn_opt_out())
+        self.event.screenshot(self.screenshot())
+        self.click(element=self.btn_ok())
+        self.event.screenshot(self.screenshot())
+        self.navigate_up()
 
 
 
