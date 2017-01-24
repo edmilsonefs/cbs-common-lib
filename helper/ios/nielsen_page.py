83c11d932941ef19a67ea799427fdec154e7c225
luisatheodoro <theodoro.luisa@gmail.com>
2017-01-24 10:33:51 +0000

    nielsen

--- helper/ios/nielsen_page.py
+++ helper/ios/nielsen_page.py
@@ -5,17 +5,20 @@ class NielsenPage(BasePage):
     def __init__(self, driver, event):
         super(NielsenPage, self).__init__(driver, event)
 
-    def lbl_title(self, timeout=10):
-        return self.top_toolbar(timeout=timeout).find_element_by_name('Nielsen Info & Your Choices')
+    # def lbl_title(self, timeout=10):
+    #     return self.top_toolbar(timeout=timeout).find_element_by_name('Nielsen Info & Your Choices')
 
     def btn_opt_out(self, timeout=10):
-        return self.get_element(timeout=timeout, xpath="//android.view.View[contains(@content-desc,'click here')][1]")
+        return self.get_element(timeout=timeout, xpath='(//UIAStaticText[@name="click here"])[1]')
 
     def btn_opt_in(self, timeout=10):
-        return self.get_element(timeout=timeout, xpath="//android.view.View[contains(@content-desc,'click here')][2]")
+        return self.get_element(timeout=timeout, xpath='(//UIAStaticText[@name="click here"])[2]')
 
     def btn_ok(self, timeout=10):
-        return self.get_element(timeout=timeout, name='OK')
+        return self.get_element(timeout=timeout, xpath='//UIAButton[@name="OK"]')
+
+    def btn_close(self, timeout=10):
+        return self.get_element(timeout=timeout, xpath='(//UIAButton[@name="Close"])')
 
 
 
