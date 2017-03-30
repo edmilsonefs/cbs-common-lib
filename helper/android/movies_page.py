fe23c409b618d5d02dabede621f512e94c17915c
romerop <GAlo5880>
2017-03-30 11:21:03 +0100

    movie popup

--- helper/android/movies_page.py
+++ helper/android/movies_page.py
@@ -8,12 +8,46 @@ class MoviesPage(BasePage):
     def lbl_title(self, timeout=10):
         return self.top_toolbar(timeout=timeout).find_element_by_xpath("//*[@text='Movies']")
 
+    def video_thumbnail(self, timeout=10):
+        return self.get_element(timeout=timeout, id=self.com_cbs_app + ":id/imgThumbnail")
+
+    def btn_movie_poster(self, timeout=10):
+        return self.get_element(timeout=timeout, id= self.com_cbs_app + ":id/movieImage")
+
+    def txt_movie_name(self, timeout=10):
+        return self.get_element(timeout=timeout, id=self.com_cbs_app + ":id/txtMovieName")
+
+    def txt_meta_data(self, timeout=10):
+        return self.get_element(timeout=timeout, id=self.com_cbs_app + ":id/txtMovieMetadata")
+
+    def txt_movie_description(self, timeout=10):
+        return self.get_element(timeout=timeout, id=self.com_cbs_app + ":id/txtMovieDescription")
+
+    def btn_subscribe_to_watch(self, timeout=10):
+        return self.get_element(timeout=timeout, name="SUBSCRIBE TO WATCH")
+
+    def btn_watch_movie(self, timeout=10):
+        return self.get_element(timeout=timeout, name="WATCH MOVIE")
+
     def validate_page(self, user_type='anonymous'):
         self.verify_exists(element=self.btn_hamburger_menu(), screenshot=True)
         self.verify_exists(element=self.img_logo())
         self.verify_exists(element=self.btn_search_icon())
         self.verify_exists(element=self.lbl_title())
+        self.verify_exists(element=self.btn_movie_poster())
         if user_type in [self.cf_subscriber, self.subscriber, self.trial]:
             pass
         else:
             pass
+
+    def movie_details_popup_validation(self, user_type='anonymous'):
+        self.verify_exists(element=self.video_thumbnail())
+        self.verify_exists(element=self.txt_movie_name())
+        self.verify_exists(element=self.txt_meta_data())
+        self.verify_exists(element=self.txt_movie_description())
+        if user_type in [self.anonymous, self.ex_subscriber, self.registered]:
+            self.verify_exists(element=self.btn_subscribe_to_watch())
+            self.verify_not_exists(element=self.btn_watch_movie())
+        else:
+            self.verify_exists(element=self.btn_watch_movie())
+            self.verify_not_exists(element=self.btn_subscribe_to_watch())
