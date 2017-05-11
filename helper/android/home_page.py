db31a218387d35a533c1397110dfec70a50fec11
denys <denys.zaiats@gmail.com>
2017-05-11 13:39:48 +0300

    [master] - added Movies deeplinking scenarios

--- helper/android/home_page.py
+++ helper/android/home_page.py
@@ -59,6 +59,7 @@ class HomePage(BasePage):
                     list_episodes = self.get_elements(name=PAID)
                     self.click(element=list_episodes[0])
                     self.safe_screenshot()
+                    self.accept_popup_video_click()
                     break
                 else:
                     count += 1
