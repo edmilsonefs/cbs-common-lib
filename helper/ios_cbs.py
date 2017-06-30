cc3de20d5d9b26efe0b488637f9c48f181078768
valdopurde <valdo@testlio.com>
2017-06-30 16:28:47 +0300

    iOS 10 fixes

--- helper/ios_cbs.py
+++ helper/ios_cbs.py
@@ -1764,6 +1764,7 @@ class CommonIOSHelper(TestlioAutomationTest):
         if self.exists(id='CONTINUE', timeout=10):
             if os.environ['AUTOMATION_NAME'] == 'XCUITest':    #iOS 10 switch
                 self.tap_element(xpath="//XCUIElementTypeButton[not(@name)]")
+                sleep(4)
             try:
                 self.tap_element(xpath="//UIAScrollView[./UIAButton[@name='CONTINUE']]//UIAButton[1]")
                 self.click(accessibility_id='CONTINUE')
