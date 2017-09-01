8dcaf533261c51ea76e5ba4784349b130baa6f31
Denys Zaiats <denys.zaiats@dreamit.de>
2017-09-01 14:37:54 +0200

    [main] - fix for Core Android popup

--- helper/cbs.py
+++ helper/cbs.py
@@ -1307,12 +1307,7 @@ class CommonHelper(TestlioAutomationTest):
             sleep(3)
         except:
             self.event._log_info(self.event._event_data('Optimum was not connected'))
-        self.navigate_up()
-        if "KFTBWI" in self.testdroid_device:
-            try:
-                self.navigate_up()
-            except:
-                pass
+        self.back_while_open_drawer_is_visible()
 
     def go_to_providers_page(self):
         self.goto_live_tv()
