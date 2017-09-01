d4c494919237d1fc71348f8797d507e010abe0d7
Denys Zaiats <denys.zaiats@dreamit.de>
2017-09-01 14:23:53 +0200

    [main] - fix for Core Android popup

--- helper/cbs.py
+++ helper/cbs.py
@@ -39,7 +39,7 @@ class CommonHelper(TestlioAutomationTest):
         self.init_variables()
 
     def init_variables(self):
-        if str(os.getenv('PLATFORM')).lower() == "android":
+        if str(os.getenv('PLATFORM')).lower() != "ios":
             if os.getenv('LOCAL') is None:
                 self.testdroid_device = self.get_testdroid_device_from_adb()
             self.activate_standard_keyboard()
