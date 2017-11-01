de1db961ad303d7fe64e60084774325da8381725
denys <denys.zaiats@gmail.com>
2017-11-01 17:37:23 +0100

    [ios] - fix for ios

--- helper/ios_cbs.py
+++ helper/ios_cbs.py
@@ -1037,15 +1037,15 @@ class CommonIOSHelper(TestlioAutomationTest):
 
         count = 0
         if self.is_element_visible(self.exists(id='Movies', timeout=6)):
-            movies = self.exists(id='Movies', timeout=6)
+            movies = self.is_element_visible(self.exists(id='Movies', timeout=6))
             while movies.location['y'] + movies.size['height'] > window_size_height / (2 if self.phone else 3) and count < 70:
                 self.swipe_down(1, (400 if self.tablet else 200))
                 count += 1
-                movies = self.exists(id='Movies', timeout=6)
+                movies = self.is_element_visible(self.exists(id='Movies', timeout=6))
 
-        self.assertTrueWithScreenShot(self.exists(id='Movies', timeout=6).is_displayed(), screenshot=True, msg='Movies Posters should be presented')
+        self.assertTrueWithScreenShot(self.is_element_visible(self.exists(id='Movies', timeout=6)), screenshot=True, msg='Movies Posters should be presented')
 
-        movies = self.exists(id='Movies', timeout=6)
+        movies = self.is_element_visible(self.exists(id='Movies', timeout=6))
 
         count = 0
         while movies.location['y'] + movies.size['height'] > window_size_height / (2 if self.phone else 3) and count < 10:
