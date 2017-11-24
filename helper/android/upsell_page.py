5512e2a01a174d1deefa72e561d8b11bb0641fb9
Denys Zaiats <dzaiats@users.noreply.github.com>
2017-11-24 17:54:17 +0100

    changed label name

--- helper/android/upsell_page.py
+++ helper/android/upsell_page.py
@@ -43,7 +43,7 @@ class UpsellPage(BasePage):
                 self.verify_exists(xpath="//*[contains(@text,'Only $5.99/month')]", timeout=20)
                 self.verify_exists(name='SELECT', timeout=20)
                 self.verify_not_exists(element=self.btn_try_1_week_month_free(), timeout=10)
-                self.verify_not_exists(element=self.btn_get_started(), timeout=10, name='TRY 3 DAYS FREE') #TODO change later
+                self.verify_not_exists(element=self.btn_get_started(), timeout=10, name='GET STARTED') #TODO change later
 
     def select_sign_in_from_text_link(self):
         self.event._log_info(self.event._event_data('Select Sign In'))
