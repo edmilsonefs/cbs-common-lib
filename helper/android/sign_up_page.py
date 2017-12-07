14d671dc2b4f63fbad54cd330a0df0c491a76b1a
Denys Zaiats <denys.zaiats@dreamit.de>
2017-12-07 15:51:42 +0100

    [main] - implemented new pause video method

--- helper/android/sign_up_page.py
+++ helper/android/sign_up_page.py
@@ -263,7 +263,7 @@ class SignUpPage(BasePage):
             self._short_swipe_down(duration=1000)
             self._short_swipe_down(duration=1000)
         elem = self.btn_already_have_an_account_sign_in()
-        elem = elem if elem is not None else self.btn_already_subscriber_sign_in()
+        elem = elem if elem else self.btn_already_subscriber_sign_in()
         self.click_by_location(elem, side='right')
         sleep(3)
         self.verify_exists(name='Sign in with your email', timeout=10)
