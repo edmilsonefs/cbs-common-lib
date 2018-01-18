961cab74b1b6d14350adb00a35b093032190da0f
luisatheodoro <theodoro.luisa@gmail.com>
2018-01-18 09:14:42 +0000

    optimum button

--- helper/ios_cbs.py
+++ helper/ios_cbs.py
@@ -2159,7 +2159,7 @@ class CommonIOSHelper(TestlioAutomationTest):
         self.click(id='VERIFY NOW', screenshot=True)
 
     def select_optimum_from_provider_page(self):
-        self.click(xpath='//UIACollectionCell[3]')
+        self.click(xpath='//XCUIElementTypeCell[3]')
 
     def go_to_optimum_page(self):
         self.go_to_providers_page()
