c1e454ecbed3cb9c1d0fc5c17d346b5fe1efea3e
denys <denys.zaiats@gmail.com>
2018-01-20 09:57:03 +0100

    [master] - testify_config.py

--- helper/testify_config.py
+++ helper/testify_config.py
@@ -20,7 +20,7 @@ buildversion = os.getenv("BUILD_VERSION")
 
 def video_profile(video_type):
     return {
-        'p_name': "{'Comscore':'" + comscore_profile + "','Omniture':'" + "OM Android CBS Ent App" + "','LEVT':'" + levt_profile + "','Heartbeat':'" + heartbit_profile + "'}",
+        'p_name': "{'Comscore':'" + comscore_profile + "','Omniture':'" + "OM " + _get_platform() + " CBS Ent App" + "','LEVT':'" + levt_profile + "','Heartbeat':'" + heartbit_profile + "'}",
         'skip_pass': skip_pass, 'email_list': "['bryan.gaikwad@cbsinteractive.com', 'joael.harbi@cbsinteractive.com']",
         'appname': appname, 'platform': _get_platform(), 'buildversion': str(buildversion), 'videotype': video_type}
 
