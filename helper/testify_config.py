20bf36cf0e0af5ae8160e6463b33aa2fd3a3a87b
Valdo <valdo@testlio.com>
2018-02-08 16:38:22 +0200

    Refactoring Testify profile creation. Added testtype

--- helper/testify_config.py
+++ helper/testify_config.py
@@ -12,11 +12,11 @@ Usage:
 1. Set the configuration in the testable class:
     - TestifyConfig.platform = 'Android' or 'iOS'
     - TestifyConfig.testify_url = '{url}'
-    
+
 2. Call method upload, as:
     - TestifyConfig.upload(TestifyConfig.video_profile({video_type})), where video type: clips, shows, live tv, movies
     - TestifyConfig.upload(TestifyConfig.sign_in_profile({omniture_profile}))
-    
+
     methods return Boolean
 """
 
@@ -25,20 +25,67 @@ platform = None
 buildversion = os.getenv("BUILD_VERSION")
 
 
-def video_profile(video_type):
-    return {
-        'p_name': "{'Omniture':'" + "OM " + platform + " CBS Ent App" + "',"
-                  "'Heartbeat':'" + "HB CBSAPP " + platform + "'}",
-        'skip_pass': "True", 'email_list': "['bryan.gaikwad@cbsinteractive.com', 'joael.harbi@cbsinteractive.com', 'kristjan@testlio.com']",
-        'appname': "CBS App", 'platform': platform, 'buildversion': str(buildversion), 'videotype': video_type}
+def video_profile(video_type, testtype):
+    # return {
+    #     'p_name': "{'Omniture':'" + "OM " + platform + " CBS Ent App" + "',"
+    #               "'Heartbeat':'" + "HB CBSAPP " + platform + "'}",
+    #     'skip_pass': "True", 'email_list': "['bryan.gaikwad@cbsinteractive.com', 'joael.harbi@cbsinteractive.com', 'kristjan@testlio.com']",
+    #     'appname': "CBS App", 'platform': platform, 'buildversion': str(buildversion), 'videotype': video_type}
+    return _get_testify_config({
+        'omniture': 'OM ' + platform + ' CBS Ent App',
+        'heartbeat': 'HB CBSAPP ' + platform,
+        'platform': platform,
+        'buildversion': str(buildversion),
+        'videotype': video_type,
+        'testtype': testtype
+        })
 
 
-def sign_in_profile(omniture_profile):
-    return {
-        'p_name': "{'Omniture':'" + omniture_profile + "',"
-                  "'Heartbeat':'" + "HB CBSAPP " + platform + "'}",
-        'skip_pass': "True", 'email_list': "['bryan.gaikwad@cbsinteractive.com', 'joael.harbi@cbsinteractive.com', 'kristjan@testlio.com']",
-        'appname': "CBS App", 'platform': platform, 'buildversion': str(buildversion)}
+def sign_in_profile(omniture_profile, testtype):
+    # return {
+    #     'p_name': "{'Omniture':'" + omniture_profile + "',"
+    #               "'Heartbeat':'" + "HB CBSAPP " + platform + "'}",
+    #     'skip_pass': "True", 'email_list': "['bryan.gaikwad@cbsinteractive.com','joael.harbi@cbsinteractive.com','kristjan@testlio.com']",
+    #     'appname': "CBS App", 'platform': platform, 'buildversion': str(buildversion)}
+    return _get_testify_config({
+        'omniture': omniture_profile,
+        'heartbeat': 'HB CBSAPP ' + platform,
+        'platform': platform,
+        'buildversion': str(buildversion),
+        'testtype': testtype
+        })
+
+
+def _get_testify_config(params=[]):
+    """Unified method for returning all variations of a Testify upload profile.
+
+    It also features built-in checks on required fields. Returns json.
+    """
+    config = {}
+
+    if 'omniture' in params and 'heartbeat' in params:
+        profiles = {'Omniture': params['omniture'],
+                    'Heartbeat': params['heartbeat']}
+    elif 'omniture' in params:
+        profiles = {'Omniture': params['omniture']}
+    elif 'heartbeat' in params:
+        profiles = {'Heartbeat': params['heartbeat']}
+    else:
+        raise Exception("At least one profile is required")
+    config['p_name'] = profiles
+
+    # config['email_list'] = ['bryan.gaikwad@cbsinteractive.com','joael.harbi@cbsinteractive.com','kristjan@testlio.com']
+    config['email_list'] = ['valdo@testlio.com','kristjan@testlio.com']
+    config['platform'] = params['platform']
+    config['appname'] = 'CBS App'
+    config['buildversion'] = str(params['buildversion'])
+    config['skip_pass'] = 'True'
+    config['testtype'] = params['testtype']
+
+    if 'videotype' in params:
+        config['videotype'] = params['video_type']
+
+    return json.dumps(config)
 
 
 def upload_dump(payload):
