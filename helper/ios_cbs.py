fd8adbcd0530e4081d206b6a0b338ae930b3999d
denys <denys.zaiats@gmail.com>
2018-02-10 16:11:54 +0100

    [master] - fix sign in ios

--- helper/ios_cbs.py
+++ helper/ios_cbs.py
@@ -1198,8 +1198,8 @@ class CommonIOSHelper(TestlioAutomationTest):
         self.safe_screenshot()
 
     def accept_video_popup(self):
-        self.click_safe(id='I ACCEPT', timeout=10)
-        self.click_safe(id='Accept', timeout=10)
+        if not self.click_safe(id='I ACCEPT', timeout=10):
+            self.click_safe(id='Accept', timeout=10)
 
     def click_watch_movie(self):
         self.click(id="Watch", timeout=7)
