b96ac6f822aee4b6eaf40e40e12aa1ec48fb0262
denys <denys.zaiats@gmail.com>
2018-02-25 15:02:53 +0100

    [master] - fix for live upsell page

--- helper/android/upsell_page.py
+++ helper/android/upsell_page.py
@@ -24,12 +24,8 @@ class UpsellPage(BasePage):
         text_list = ['allAccessLogo']
         if user_type in [self.anonymous, self.registered]:
             text_list.append('LIMITED COMMERCIALS')
-            text_list.append('TRY \d+ (WEEK|WEEKS|MONTH|MONTHS) FREE')
             text_list.append('COMMERCIAL FREE')
-            if self.IS_AMAZON:
-                text_list.append('GET STARTED')
-            elif self.IS_ANDROID:
-                text_list.append('TRY \d+ (DAY|DAYS) FREE')
+            text_list.append('GET STARTED|TRY \d+ (WEEK|WEEKS|MONTH|MONTHS) FREE')
             #if user_type == self.registered:
                 #text_list.append('SELECT') #todo: Does this exist? Doesn't seem like it
         elif user_type in [self.subscriber, self.trial]:
