486adab8a24857a12802042ab50122358a6852b7
denys <denys.zaiats@gmail.com>
2018-02-25 12:55:58 +0100

    [master] - fix for live tv validation

--- helper/android/live_tv_page.py
+++ helper/android/live_tv_page.py
@@ -127,11 +127,7 @@ class LiveTvPage(BasePage):
             self.click_allow_popup()
 
         self.verify_exists(element=self.lbl_title())
-        if self.phone:
-            text_list_one = ['Open navigation drawer']
-        else:
-            text_list_one = ['Navigate up']
-        self.verify_in_batch(text_list_one, False)
+        self.verify_in_batch(['Navigate up|Open navigation drawer'], False)
         self._short_swipe_up()
         self._short_swipe_up()
         text_list_two = []
