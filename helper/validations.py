f96a4ccac28f0ea8d6562213ef649a7fccc71f07
denys <denys.zaiats@gmail.com>
2018-03-02 08:48:10 +0100

    [user-account] - fix for scenarios

--- helper/validations.py
+++ helper/validations.py
@@ -358,24 +358,18 @@ class Validations(CommonHelper, CommonIOSHelper):
             self.upsell_page_android.validate_page(user_type=user_type)
         elif self.IS_IOS:
             if user_type in [self.anonymous, self.registered]:
-                self.verify_exists(
-                    id="LIMITED COMMERCIALS",
-                    screenshot=True, timeout=20)
+                self.verify_in_batch(['LIMITED COMMERCIALS'])
             if user_type == self.registered:
                 self.verify_not_exists(name='SELECT', timeout=10)
             elif user_type in [self.subscriber, self.trial]:
-                self.verify_exists(id="COMMERCIAL FREE")
+                self.verify_in_batch(['COMMERCIAL FREE'])
                 self.verify_exists(xpath="//XCUIElementTypeStaticText[contains(@name,'UPGRADE')]")
             elif user_type == self.cf_subscriber:
-                self.verify_exists(id="COMMERCIAL FREE")
+                self.verify_in_batch(['COMMERCIAL FREE'])
             else:
                 if user_type == self.ex_subscriber:
-                    self.verify_exists(
-                        id="LIMITED COMMERCIALS",
-                        timeout=20)
-                    self.verify_exists(id="COMMERCIAL FREE", timeout=20)
+                    self.verify_in_batch(['LIMITED COMMERCIALS', 'COMMERCIAL FREE', 'SELECT'])
                     self.verify_exists(xpath="//XCUIElementTypeStaticText[contains(@name,'Only $5.99/month')]", timeout=20)
-                    self.verify_exists(id='SELECT', timeout=20)
                     self.verify_not_exists(id='GET STARTED', timeout=10)
                     self.verify_not_exists(xpath="//XCUIElementTypeButton[contains(@name,'TRY 1') and contains(@name, 'FREE') and (contains(@name, 'MONTH') or contains(@name, 'FREE'))]", timeout=10)
 
