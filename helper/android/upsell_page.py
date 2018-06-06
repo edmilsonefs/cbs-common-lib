59d1e105cce4627fd58299616c5075281b6bd560
luisatheodoro <theodoro.luisa@gmail.com>
2018-06-06 15:10:43 +0100

    update validation V

--- helper/android/upsell_page.py
+++ helper/android/upsell_page.py
@@ -27,23 +27,24 @@ class UpsellPage(BasePage):
             text_list.append('LIMITED COMMERCIALS')
             text_list.append('COMMERCIAL FREE')
             text_list.append('GET STARTED|TRY \d+ (WEEK|WEEKS|MONTH|MONTHS) FREE')
-            #if user_type == self.registered:
-                #text_list.append('SELECT') #todo: Does this exist? Doesn't seem like it
         elif user_type in [self.subscriber, self.trial]:
             text_list.append('COMMERCIAL FREE')
             text_list.append('UPGRADE')
+            text_list.append('READ OUR FAQ')
         elif user_type == self.cf_subscriber:
             text_list.append('COMMERCIAL FREE')
+            text_list.append('READ OUR FAQ')
+            self.verify_not_exists(name='UPGRADE')
         else:
             if user_type == self.ex_subscriber:
                 text_list.append('LIMITED COMMERCIALS')
                 text_list.append('COMMERCIAL FREE')
-                #text_list.append('Only \$5\.99\/month')
+                text_list.append('Only \$5\.99\/month')
+                text_list.append('Only \$9\.99\/month')
                 text_list.append('SELECT')
-                #text_list.append('TRY 1 MONTH FREE') Doesn't exist on page, design change?
-                #text_list.append('GET STARTED') See above
 
-        self.verify_in_batch(text_list, case_sensitive=False, strict_visibility=True, screenshot=True, strict=False, with_timeout=20)
+        self.verify_in_batch(text_list, case_sensitive=False, strict_visibility=True, screenshot=True, strict=False,
+                             with_timeout=20)
 
     def select_sign_in_from_text_link(self):
         self.event._log_info(self.event._event_data('Select Sign In'))
